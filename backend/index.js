f41471b99cb3116e6b77e5e675164b31efb2b89a
Pawan <pawan@MacBookAir.lan>
2026-07-15 16:34:33 +0530

    fixed issue

--- backend/index.js
+++ backend/index.js
@@ -47,39 +47,39 @@ app.use("/api/blog", blogRoutes);
 app.use("/api/contact", getInTouch);
 app.use("/api/global", globalSearchRoute);
 
-connectToDB();
+await connectToDB();
 
-app.delete("/api/delete-all", async (req, res) => {
-  try {
-    const db = mongoose.connection.db;
+// app.delete("/api/delete-all", async (req, res) => {
+//   try {
+//     const db = mongoose.connection.db;
 
-    const collections = await db.listCollections().toArray();
+//     const collections = await db.listCollections().toArray();
 
-    const result = [];
+//     const result = [];
 
-    for (const collection of collections) {
-      if (!collection.name.startsWith("system.")) {
-        const deleteResult = await db.collection(collection.name).deleteMany({});
+//     for (const collection of collections) {
+//       if (!collection.name.startsWith("system.")) {
+//         const deleteResult = await db.collection(collection.name).deleteMany({});
 
-        result.push({
-          collection: collection.name,
-          deletedCount: deleteResult.deletedCount,
-        });
-      }
-    }
+//         result.push({
+//           collection: collection.name,
+//           deletedCount: deleteResult.deletedCount,
+//         });
+//       }
+//     }
 
-    res.status(200).json({
-      success: true,
-      message: "All collection data deleted successfully",
-      result,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+//     res.status(200).json({
+//       success: true,
+//       message: "All collection data deleted successfully",
+//       result,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       error: error.message,
+//     });
+//   }
+// });
 
 // app.listen(PORT, () => {
 //   console.log(`server is now running on port ${PORT}`);
